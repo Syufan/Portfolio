d952088fd4bbfb98d6672ac2ae2610de55cd2b7b
Syufan <zyf571635687@gmail.com>
2026-03-06 00:09:47 +0800

    test: update tests to use directory-based db path

--- chatboat/src/domain/test/test_application.py
+++ chatboat/src/domain/test/test_application.py
@@ -5,6 +5,7 @@ from src.domain.application import Application
 def test_successfully_open_suggestions_file(tmp_path):
     fake_path = tmp_path / "suggestions.yaml"
     fake_path.write_text("suggestions:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
+    (tmp_path / "portfolio.yaml").write_text("some data")
 
     assert Application(tmp_path)._load_suggestions(fake_path)==['hello','hey','okay']
 
@@ -23,19 +24,21 @@ def test_less_than_3_suggestions(tmp_path):
 def test_pick_3_suggestions(tmp_path):
     fake_path = tmp_path / "suggestions.yaml"
     fake_path.write_text("suggestions:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
+    (tmp_path / "portfolio.yaml").write_text("some data")
 
     assert len(Application(tmp_path).pick_random_suggestion())==3
 
-
 def test_successfully_open_data_file(tmp_path):
-    fake_path = tmp_path / "data.yaml"
+    fake_path = tmp_path / "portfolio.yaml"
     fake_path.write_text("data:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
+    (tmp_path / "suggestions.yaml").write_text("suggestions:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
 
     assert Application(tmp_path)._load_data(fake_path) != ""
 
 def test_open_empty_data_file(tmp_path):
-    fake_path = tmp_path / "data.yaml"
-    fake_path.write_text("data:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
+    fake_path = tmp_path / "portfolio.yaml"
+    fake_path.write_text("")
+    (tmp_path / "suggestions.yaml").write_text("suggestions:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
 
     with pytest.raises(ValueError):
         Application(tmp_path)._load_data(fake_path)
