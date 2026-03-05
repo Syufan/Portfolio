bdd3ebcd6f922446b1ad0812e0fbf2a8ecf9fd1c
Syufan <zyf571635687@gmail.com>
2026-03-05 23:59:01 +0800

    test: add load data test

--- chatboat/src/domain/test/test_application.py
+++ chatboat/src/domain/test/test_application.py
@@ -6,23 +6,36 @@ def test_successfully_open_suggestions_file(tmp_path):
     fake_path = tmp_path / "suggestions.yaml"
     fake_path.write_text("suggestions:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
 
-    assert Application(fake_path)._load_suggestions(fake_path)==['hello','hey','okay']
+    assert Application(tmp_path)._load_suggestions(fake_path)==['hello','hey','okay']
 
 def test_fail_open_suggestions_file(tmp_path):
-    fake_path = tmp_path / "suggestions.yaml"
 
     with pytest.raises(FileNotFoundError):
-        Application(fake_path)
+        Application(tmp_path)
 
 def test_less_than_3_suggestions(tmp_path):
     fake_path = tmp_path / "suggestions.yaml"
     fake_path.write_text("suggestions:\n  - 'hello'\n  - 'hey'")
 
     with pytest.raises(ValueError):
-        Application(fake_path)
+        Application(tmp_path)
 
 def test_pick_3_suggestions(tmp_path):
     fake_path = tmp_path / "suggestions.yaml"
     fake_path.write_text("suggestions:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
 
-    assert len(Application(fake_path).pick_random_suggestion())==3
+    assert len(Application(tmp_path).pick_random_suggestion())==3
+
+
+def test_successfully_open_data_file(tmp_path):
+    fake_path = tmp_path / "data.yaml"
+    fake_path.write_text("data:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
+
+    assert Application(tmp_path)._load_data(fake_path) != ""
+
+def test_open_empty_data_file(tmp_path):
+    fake_path = tmp_path / "data.yaml"
+    fake_path.write_text("data:\n  - 'hello'\n  - 'hey'\n  - 'okay'")
+
+    with pytest.raises(ValueError):
+        Application(tmp_path)._load_data(fake_path)
