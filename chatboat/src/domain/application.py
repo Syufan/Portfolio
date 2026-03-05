10ab87aa3308ed439125af569aaab449c366e2ce
Syufan <zyf571635687@gmail.com>
2026-03-06 00:10:51 +0800

    feat: load data function

--- chatboat/src/domain/application.py
+++ chatboat/src/domain/application.py
@@ -1,11 +1,14 @@
 import yaml
 import random
+from pathlib import Path
 
 SUGGESTIONS_LIMIT = 3
 
 class Application:
     def __init__(self, db_path: str) -> None:
-        self._suggestions = self._load_suggestions(db_path)
+        path = Path(db_path)
+        self._suggestions = self._load_suggestions(path / "suggestions.yaml")
+        self._data = self._load_data(path / "portfolio.yaml")
 
 
     def _load_suggestions(self, db_path:str) -> list[str]:
@@ -15,6 +18,12 @@ class Application:
             raise ValueError("Not enough suggestions")
         return data["suggestions"]
 
+    def _load_data(self, db_path:str) -> str:
+        with open(db_path) as f:
+            data = f.read()
+        if not data:
+            raise ValueError("File is empty")
+        return data
 
     def pick_random_suggestion(self) -> list[str]:
         return random.sample(self._suggestions, SUGGESTIONS_LIMIT)
